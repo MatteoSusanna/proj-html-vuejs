0f267865fe7bd6aa16f3e6dacb859352137b3203
Matteo Susanna <matteo2784@gmail.com>
2022-09-05 15:46:29 +0200

    Fine primo slider

--- src/components/SectionOverview.vue
+++ src/components/SectionOverview.vue
@@ -21,13 +21,22 @@
             </div>
         </div>
 
+        <!-- primo slider -->
         <div class="col">
             <div class="wrapper-slider1">
+                <!-- immagine attiva -->
                 <div class="img-active">
-                    <img src="" alt="">
+                    <img :src=" require('../assets/img/gallery_0' + counterSlider + '.jpg')" alt="bambino">
+                        <!-- freccie avanti indietro-->
+                    <div class="next" @click="next"><img src="../assets/img/slider_next.png" alt="freccia avanti"></div>
+                    <div class="pre" @click="prev"><img src="../assets/img/slider_previous.png" alt="freccia indietro"></div>
                 </div>
-                <div class="list-img">
 
+                <!-- lista immagini -->
+                <div class="list-img">
+                    <div class="card-img" v-for="(img, indice) in slider1Array" :key="indice">
+                        <img :src="img.url" alt="bambino" @click="counterSlider = indice">
+                    </div>
                 </div>
             </div>
         </div>
@@ -40,6 +49,7 @@ export default {
     nome: 'SectionOverview',
     data(){
         return{
+            counterSlider: 0,
             overviewArray:  [
                                 {
                                     'url': require('../assets/img/clock_alt.png'),
@@ -51,7 +61,38 @@ export default {
                                     'titolo': 'VariedClasses',
                                     'descrizione': 'Pulvinar est metro ligula bladt maecenas retrum gravida cuprum. Maecenas node estibulum.',
                                 }
+                            ],
+            slider1Array:   [
+                                {
+                                    'url': require('../assets/img/gallery_00.jpg')
+                                },
+                                {
+                                    'url': require('../assets/img/gallery_01.jpg')
+                                },
+                                {
+                                    'url': require('../assets/img/gallery_02.jpg')
+                                },
+                                {
+                                    'url': require('../assets/img/gallery_03.jpg')
+                                },
                             ]
+        }
+    },
+    methods:{
+        next(){
+            if(this.counterSlider == this.slider1Array.length - 1){
+                this.counterSlider = 0;
+            }else{
+                this.counterSlider++;
+            }
+        },
+        prev(){
+            
+            if(this.counterSlider == 0){
+                this.counterSlider = this.slider1Array.length - 1;
+            }else{
+                this.counterSlider--;
+            }
         }
     }
 }
@@ -118,15 +159,37 @@ h2, h4{
             flex-direction: column;
             .img-active{
                 height: 80%;
+                position: relative;
                 img{
                     width: 100%;
+                }
+
+                .next, .pre{
+                    width: 40px;
+                    height: 40px;
+                    background-color: $blaze_orange;
+                    position: absolute;
+                }
+                .pre{
+                    top: calc(50% - 20px);
+                }
+                .next{
+                    top: calc(50% - 20px);
+                    left: calc(100% - 40px);
                 }
             }
             .list-img{
                 height: 20%;
-                img{
-                    width: 20%
+                width: 100%;
+                display: flex;
+                justify-content: space-between;
+                .card-img{
+                    width: calc(100% / 3 - 0.2rem);
+                    img{
+                        width: 100%;
+                    }
                 }
+                
             }
         }
     }
